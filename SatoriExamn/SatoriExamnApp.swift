eaefd409384cb898d81d136d883622864ec8aedc
AlejandroCasrico <casiricoalex@outlook.com>
2023-11-28 06:50:58 -0600

    Initial Commit

---
+++ SatoriExamn/SatoriExamnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatoriExamnApp.swift
+//  SatoriExamn
+//
+//  Created by Alejandro Casillas  on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SatoriExamnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
